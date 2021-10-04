b4aad6e30fd8c0a2b0010582da8c74e5a689c51b
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-10-04 13:46:41 -0300

    adding HUD

--- HUD_V1.2/Program.cs
+++ HUD_V1.2/Program.cs
@@ -8,8 +8,70 @@ namespace HUD_V1._2
 {
 	class Program
 	{
+		static int Health = 100;
+		static int Pistol = 0;
+		static int ShotGun = 1;
+		static int Spreader = 2;
+		static int Laser = 3;
+		static int Sniper = 4;
+		static int BFG = 4;
+		static int Lives = 3;
+
 		static void Main(string[] args)
 		{
+
+		}
+
+		void health()
+		{
+			if (Health > 100)
+			{
+				Console.WriteLine("You are healthy");
+			}
+			if (Health < 75)
+			{
+				Console.WriteLine("Nothing but a scratch");
+			}
+
+			if (Health < 50)
+			{
+				Console.WriteLine("Hurt");
+			}
+			if (Health < 25)
+			{
+				Console.WriteLine("OOOO The Pain!!");
+			}
+			if (Health < 10)
+			{
+				Console.WriteLine("Heal you madman");
+			}
+			if (Health < 0)
+			{
+				Lives = Lives - 1;
+				Console.WriteLine("You have died");
+			}
+		}
+
+
+		void Damage()
+		{
+			
+
+
+		}
+
+		static void Weapon()
+		{
+			
+
+		}
+		void ShowHUD()
+		{
+
+			Console.WriteLine("Health - " + Health);
+			//Console.WriteLine("Current weapon - " + Weapon);
+			Console.WriteLine("Health - " + Health);
+
 		}
 	}
 }
